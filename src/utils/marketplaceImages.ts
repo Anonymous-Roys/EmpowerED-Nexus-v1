6067cc68c4d0788dda4b0118c23685b5b35f925d
Arhin David Kwabena <davidarhin2005@gmail.com>
2026-01-13 19:50:00 -0500

    images from jpg to png

--- src/utils/marketplaceImages.ts
+++ src/utils/marketplaceImages.ts
@@ -7,6 +7,26 @@ import msiThin2 from '../assets/marketplace-products/msi-thin2.png';
 import msiThin3 from '../assets/marketplace-products/msi-thin3.png';
 import hpProbook1 from '../assets/marketplace-products/hp-probook1.png';
 import hpProbook2 from '../assets/marketplace-products/hp-probook2.png';
+import razer1 from '/src/assets/marketplace-products/razer-blade-1.png';
+import razer2 from '../assets/marketplace-products/razer-blade-2.png';
+import razer3 from '../assets/marketplace-products/razer-blade-3.png';
+import predatorTriton1 from '../assets/marketplace-products/predator-triton-1.png';
+import predatorTriton2 from '../assets/marketplace-products/predator-triton-2.png';
+import predatorTriton3 from '../assets/marketplace-products/predator-triton-3.png';
+import hpEliteBookX2 from '../assets/marketplace-products/HP_ELITEBOOK_x3830G7_2.png';
+import hpEliteBookX3 from '../assets/marketplace-products/HP_ELITEBOOK_x3830G7_3.png';
+import hpEliteBookX4 from '../assets/marketplace-products/HP_ELITEBOOK_x3830G7_4.png';
+import surfaceBook1 from '../assets/marketplace-products/surface-book-1.png';
+import surfaceBook2 from '../assets/marketplace-products/surface-book-2.png';
+import surfaceBook3 from '../assets/marketplace-products/surface-book-3.png';
+import thinkPad1 from '../assets/marketplace-products/thinkpad-yoga-1.png';
+import thinkPad2 from '../assets/marketplace-products/thinkpad-yoga-2.png';
+import thinkPad3 from '../assets/marketplace-products/thinkpad-yoga-3.png';
+import thinkPad4 from '../assets/marketplace-products/thinkpad-yoga-4.png';
+import eliteBook840G1 from '../assets/marketplace-products/hp-elitebook-840-g8-1.png';
+import eliteBook840G2 from '../assets/marketplace-products/hp-elitebook-840-g8-2.png';
+import eliteBook840G3 from '../assets/marketplace-products/hp-elitebook-840-g8-3.png';
+
 
 export const marketplaceImages = {
   hpZbook1,
@@ -18,4 +38,23 @@ export const marketplaceImages = {
   hpProbook1,
   hpProbook2,
   hpProbook3: hpProbook2, // Fallback since hp-probook3.png is missing
+  razer1,
+  razer2,
+  razer3,
+  predatorTriton1,
+  predatorTriton2,
+  predatorTriton3,
+  hpEliteBookX2,
+  hpEliteBookX3,
+  hpEliteBookX4,
+  surfaceBook1,
+  surfaceBook2,
+  surfaceBook3,
+  thinkPad1,
+  thinkPad2,
+  thinkPad3,
+  thinkPad4,
+  eliteBook840G1,
+  eliteBook840G2,
+  eliteBook840G3
 };
